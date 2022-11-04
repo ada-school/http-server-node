6bc62ec003107418fad20825f8ed8ef1d549ff0f
Sergio Marentes <sergiomarentes705@gmail.com>
2022-11-04 09:08:59 -0500

    fixing tech challenge solutions

--- servidor.js
+++ servidor.js
@@ -1,27 +1,27 @@
 const http = require("http");
-const fs = require("fs").promises;
+const fs = require("fs");
 const host = "localhost";
 const port = 8000;
 const requestListener = function (req, res) {
   const url = new URL(req.url, `https://localohst:${port}/`);
+
   res.setHeader("Content-Type", "text/html");
-  res.writeHead(200);
-  fs.readFile(__dirname + "/realcar.html")
-    .then((contents) => {
-      res.end(contents);
-    })
-    .catch((err) => {
+  fs.readFile(__dirname + "/realcar.html", "utf-8", (err, data) => {
+    if (err) {
       res.writeHead(500);
-      res.end(err);
-      return;
-    });
+      res.write("file not found");
+    } else {
+      res.writeHead(200);
+      res.write(data);
+    }
+    res.end();
+  });
   if (url.pathname === "/user") {
     const name = url.searchParams.get("name");
     const email = url.searchParams.get("email");
 
     //res.write(JSON.stringify({ nombre: name, email: email }));
   }
-  res.end();
 };
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
