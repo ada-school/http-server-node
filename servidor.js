a9750269ff82d8b2f8430b7c75118b9e716e38bd
Sergio Marentes <sergiomarentes705@gmail.com>
2022-11-04 09:15:21 -0500

    adding edad param

--- servidor.js
+++ servidor.js
@@ -6,21 +6,36 @@ const requestListener = function (req, res) {
   const url = new URL(req.url, `https://localohst:${port}/`);
 
   res.setHeader("Content-Type", "text/html");
-  fs.readFile(__dirname + "/realcar.html", "utf-8", (err, data) => {
-    if (err) {
-      res.writeHead(500);
-      res.write("file not found");
-    } else {
-      res.writeHead(200);
-      res.write(data);
-    }
-    res.end();
-  });
-  if (url.pathname === "/user") {
-    const name = url.searchParams.get("name");
-    const email = url.searchParams.get("email");
 
-    //res.write(JSON.stringify({ nombre: name, email: email }));
+  if (url.pathname === "/user") {
+    const edad = url.searchParams.get("edad");
+    if (edad < 18) {
+      fs.readFile(__dirname + "/toycar.html", "utf-8", (err, data) => {
+        if (err) {
+          res.writeHead(500);
+          res.write("file not found");
+        } else {
+          res.writeHead(200);
+          res.write(data);
+        }
+        res.end();
+      });
+    } else {
+      fs.readFile(__dirname + "/realcar.html", "utf-8", (err, data) => {
+        if (err) {
+          res.writeHead(500);
+          res.write("file not found");
+        } else {
+          res.writeHead(200);
+          res.write(data);
+        }
+        res.end();
+      });
+    }
+  } else {
+    res.writeHead(400);
+    res.write("use the correct path");
+    res.end();
   }
 };
 const server = http.createServer(requestListener);
