d41ab0ed1b3e76f9e81665f57695ece8807fc157
Sergio Marentes <sergiomarentes705@gmail.com>
2022-11-04 08:46:18 -0500

    adding html response

--- servidor.js
+++ servidor.js
@@ -1,13 +1,25 @@
 const http = require("http");
+const fs = require("fs").promises;
 const host = "localhost";
 const port = 8000;
 const requestListener = function (req, res) {
   const url = new URL(req.url, `https://localohst:${port}/`);
+  res.setHeader("Content-Type", "text/html");
+  res.writeHead(200);
+  fs.readFile(__dirname + "/realcar.html")
+    .then((contents) => {
+      res.end(contents);
+    })
+    .catch((err) => {
+      res.writeHead(500);
+      res.end(err);
+      return;
+    });
   if (url.pathname === "/user") {
     const name = url.searchParams.get("name");
     const email = url.searchParams.get("email");
-    res.writeHead(200);
-    res.write(JSON.stringify({ nombre: name, email: email }));
+
+    //res.write(JSON.stringify({ nombre: name, email: email }));
   }
   res.end();
 };
